d927fc7ef1b7ebdbeed4cc880334d3e71e26203e
Rael Beatriz Nabong <105956144+raelbeatriz@users.noreply.github.com>
2022-06-05 09:17:50 +0800

    Update ESPtable2.sql

--- ESPtable2.sql
+++ ESPtable2.sql
@@ -2,10 +2,7 @@
 -- version 4.6.6deb4
 -- https://www.phpmyadmin.net/
 --
--- Servidor: db5000128127.hosting-data.io
--- Tiempo de generación: 18-07-2019 a las 16:50:19
--- Versión del servidor: 5.7.25-log
--- Versión de PHP: 7.0.33-0+deb9u3
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,14 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `dbs122647`
---
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `ESPtable2`
---
 
 CREATE TABLE `ESPtable2` (
   `id` int(5) NOT NULL,
@@ -38,28 +28,18 @@ CREATE TABLE `ESPtable2` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Volcado de datos para la tabla `ESPtable2`
---
+
 
 INSERT INTO `ESPtable2` (`id`, `PASSWORD`, `RECEIVED_BOOL1`, `RECEIVED_BOOL2`, `RECEIVED_BOOL3`, `RECEIVED_BOOL4`, `RECEIVED_BOOL5`, `RECEIVED_BOOL6`) VALUES
 (99999, 12345, 0, 0, 0, 0, 0, 0);
 
 --
--- Índices para tablas volcadas
---
 
---
--- Indices de la tabla `ESPtable2`
 --
 ALTER TABLE `ESPtable2`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
 
---
--- AUTO_INCREMENT de la tabla `ESPtable2`
 --
 ALTER TABLE `ESPtable2`
   MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100000;
